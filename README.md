41f391dccf874bc4d96e241c00048ff613a6f4d7
elvece <lvcifferello@gmail.com>
2015-08-04 19:21:47 -0600

    passed first 4 tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ##JS Methods, This, and Constructors
 
-***This repo contains my solution to the following exercise:***
+***This repo contains my solution to the following exercise:***3
 
 ### Setup Instructions
 
--- book_reader.js
+++ book_reader.js
@@ -1,13 +1,26 @@
-function BookReader(book) {
-  // your code here
+function BookReader(book, currentPage) {
+  this.book = book;
+  this.currentPage = 0;
 }
 
 BookReader.prototype.nextPage = function() {
-  // your code here
+  if (this.currentPage < this.book.length -1){
+    this.currentPage += 1;
+    return this.book[this.currentPage];
+  }
+  else {
+    return this.book[this.currentPage];
+  }
 };
 
 BookReader.prototype.previousPage = function() {
-  // your code here
+  if (this.currentPage > this.book.length -1){
+    this.currentPage -= 1;
+    return this.book[this.currentPage];
+  }
+  else {
+    return this.book[this.currentPage];
+  }
 };
 
 BookReader.prototype.pagesLeft = function() {
